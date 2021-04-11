8220466df384aa5b48d6102d7962fb4a298f294c
PupJoey <james.lawrence@live.co.uk>
2021-04-11 12:09:54 +0100

    Inital Commit

---
+++ src/main/java/dev/odionwolf/weather/Weather.java
@@ -0,0 +1,17 @@
+package dev.odionwolf.weather;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Weather extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
