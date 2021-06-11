253fa0ebe813dd5176e10f2e9c05032dd4ad6b79
Joey <james.lawrence@live.co.uk>
2021-06-11 10:53:25 +0100

    Removed schedular

--- src/main/java/dev/odionwolf/realisticweather/thirst/ThirstManager.java
+++ src/main/java/dev/odionwolf/realisticweather/thirst/ThirstManager.java
@@ -7,6 +7,7 @@ import org.bukkit.Material;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
